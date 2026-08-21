20c32c59a0b44dc7fdded4e106b409910608ec83
hibarikyouya810-a11y <hibarikyouya.810@gmail.com>
2026-08-21 14:56:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,27 @@
-// オフライン動作用の最小サービスワーカー
-const CACHE = 'fm-v2';
+// ネットワーク優先のサービスワーカー
+// オンライン時は常に最新を取得し、オフライン時のみキャッシュを使う
+const CACHE = 'fm-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './vendor/zxing.min.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });

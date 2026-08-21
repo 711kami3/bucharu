5e77b8857bf9e0b69c6aa72ba9da89ca7655d710
hibarikyouya810-a11y <hibarikyouya.810@gmail.com>
2026-08-21 14:41:28 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+// オフライン動作用の最小サービスワーカー
+const CACHE = 'fm-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './vendor/zxing.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});

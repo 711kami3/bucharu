38757770ed5044928ccb24dce8c2a504054f4ce9
711kami3 <hibarikyouya.810@gmail.com>
2026-08-26 15:32:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ネットワーク優先のサービスワーカー
 // オンライン時は常に最新を取得し、オフライン時のみキャッシュを使う
-const CACHE = 'fm-v4';
+const CACHE = 'fm-v5';
 const ASSETS = [
   './', './index.html', './manifest.json',
   './vendor/zxing.min.js',

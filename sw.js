d3a43df1f253d89c2197ae4577b92ad5605b9aee
711kami3 <hibarikyouya.810@gmail.com>
2026-09-01 21:35:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ネットワーク優先のサービスワーカー
 // オンライン時は常に最新を取得し、オフライン時のみキャッシュを使う
-const CACHE = 'fm-v26';
+const CACHE = 'fm-v27';
 const ASSETS = [
   './', './index.html', './terms.html', './manifest.json',
   './fuji-normal.png', './fuji-surprise.png', './fuji-scold.png', './fuji-sleepy.png',
